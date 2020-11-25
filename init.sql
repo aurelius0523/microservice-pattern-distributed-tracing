e04feb16d663f4c2aa3e8a5a5d19fc4b12e112c3
aurelius0523 <aurelius0523@gmail.com>
2020-11-26 01:25:03 +0800

    Update init.sql location for compose

--- init.sql
+++
@@ -1,4 +0,0 @@
-create table user_table ( id varchar constraint user_table_pk primary key, name varchar );
-insert into user_table values (1, 'Kim');
-insert into user_table values (2, 'Jat');
-insert into user_table values (3, 'Siva');
